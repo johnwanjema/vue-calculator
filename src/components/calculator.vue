5ddb0bd37c0fd6d3ced48c1b68de8e4b88eb54ef
John Wanjema <jonwanjema@gmail.com>
2022-04-03 19:16:39 -0700

    add function to add decimal point

--- src/components/calculator.vue
+++ src/components/calculator.vue
@@ -36,20 +36,20 @@
     <div @click="sign" class="btn">+/-</div>
     <div @click="percent" class="btn">%</div>
     <div class="btn operator">/</div>
-    <div @click="append(7)" class="btn">7</div>
-    <div @click="append(8)" class="btn">8</div>
-    <div @click="append(9)" class="btn">9</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
     <div class="btn operator">*</div>
-    <div @click="append(4)" class="btn">4</div>
-    <div @click="append(5)" class="btn">5</div>
-    <div @click="append(6)" class="btn">6</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
     <div class="btn operator">-</div>
-    <div @click="append(1)" class="btn">1</div>
-    <div @click="append(2)" class="btn">2</div>
-    <div @click="append(3)" class="btn">3</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
     <div class="btn operator">+</div>
-    <div @click="append(0)" class="zero btn">0</div>
-    <div class="btn">.</div>
+    <div @click="append('0')" class="zero btn">0</div>
+    <div @click="dot" class="btn">.</div>
     <div class="btn operator" operator>=</div>
   </div>
 </template>
@@ -58,7 +58,7 @@
 export default {
   data() {
     return{
-      current:'600000',
+      current:'',
     }
   },
   methods:{
@@ -73,7 +73,12 @@ export default {
         this.current = parseFloat(this.current / 100) 
       },
       append(number){
-        this.current = `${this.current}${number}`
+        this.current = `${this.current}${number}`;
+      },
+      dot(){
+        if (this.current.indexOf('.') === -1){
+          this.append('.')
+        }
       }
   }
 }
