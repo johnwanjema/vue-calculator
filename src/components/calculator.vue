3f90776e614ede5c92c88cca30b34ee9e5184f28
John Wanjema <jonwanjema@gmail.com>
2022-04-03 19:48:46 -0700

    add operators for calculator

--- src/components/calculator.vue
+++ src/components/calculator.vue
@@ -31,56 +31,93 @@
 
 <template>
   <div class="calculator">
-    <div class="display">{{current || '0'}}</div>
+    <div class="display">{{ current || "0" }}</div>
     <div @click="clear" class="btn">C</div>
     <div @click="sign" class="btn">+/-</div>
     <div @click="percent" class="btn">%</div>
-    <div class="btn operator">/</div>
+    <div @click="divide" class="btn operator">/</div>
     <div @click="append('7')" class="btn">7</div>
     <div @click="append('8')" class="btn">8</div>
     <div @click="append('9')" class="btn">9</div>
-    <div class="btn operator">*</div>
+    <div @click="multiply" class="btn operator">*</div>
     <div @click="append('4')" class="btn">4</div>
     <div @click="append('5')" class="btn">5</div>
     <div @click="append('6')" class="btn">6</div>
-    <div class="btn operator">-</div>
+    <div @click="minus" class="btn operator">-</div>
     <div @click="append('1')" class="btn">1</div>
     <div @click="append('2')" class="btn">2</div>
     <div @click="append('3')" class="btn">3</div>
-    <div class="btn operator">+</div>
+    <div @click="add" class="btn operator">+</div>
     <div @click="append('0')" class="zero btn">0</div>
     <div @click="dot" class="btn">.</div>
-    <div class="btn operator" operator>=</div>
+    <div @click="equals" class="btn operator" operator>=</div>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return{
-      current:'',
-    }
+    return {
+      current: "",
+      operator: null,
+      previous: null,
+      operatorClicked: false,
+    };
   },
-  methods:{
-      clear(){
-        this.current = '';
-      },
-      sign(){
-        this.current = this.current.charAt(0) === '-'?  this.current.slice(1): `-${this.current}`
-      },
-      percent(){
-        // alert('we')
-        this.current = parseFloat(this.current / 100) 
-      },
-      append(number){
-        this.current = `${this.current}${number}`;
-      },
-      dot(){
-        if (this.current.indexOf('.') === -1){
-          this.append('.')
-        }
+  methods: {
+    clear() {
+      this.current = "";
+    },
+    sign() {
+      this.current =
+        this.current.charAt(0) === "-"
+          ? this.current.slice(1)
+          : `-${this.current}`;
+    },
+    percent() {
+      // alert('we')
+      this.current = parseFloat(this.current / 100);
+    },
+    append(number) {
+      if (this.operatorClicked) {
+        this.current = "";
+        this.operatorClicked = false;
       }
-  }
-}
+      this.current = `${this.current}${number}`;
+    },
+    dot() {
+      if (this.current.indexOf(".") === -1) {
+        this.append(".");
+      }
+    },
+    divide() {
+      this.setPrevious();
+      this.operator = (a, b) => a / b;
+    },
+    multiply() {
+      this.setPrevious();
+      this.operator = (a, b) => a * b;
+    },
+    minus() {
+      this.setPrevious();
+      this.operator = (a, b) => a - b;
+    },
+    add() {
+      this.setPrevious();
+      this.operator = (a, b) => a + b;
+    },
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    equals() {
+      this.current = `${this.operator(
+        parseFloat(this.current),
+        parseFloat(this.previous)
+      )}`;
+      this.previous = null
+    },
+  },
+};
 </script>
 
